787ce3362d28770956e12ff3c49f5ab6e08a3b32
Chace Daniels <chace.daniels@outsystems.com>
2025-12-12 16:15:59 -0600

    chore: Release v8.0.0

--- packages/intune/Package.swift
+++ packages/intune/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "IonicEnterpriseIntune",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(
             name: "IonicEnterpriseIntune",
